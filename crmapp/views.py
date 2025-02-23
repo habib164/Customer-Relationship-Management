1162ee8484a62d0fc72f520aa32d25bbf17f5c4f
habib164 <foursevenonethree@gmail.com>
2025-02-24 03:36:51 +0700

    second commit

--- crmapp/views.py
+++ crmapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            messages.success(request, "You have logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error. Try again")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You logged out")
+    return redirect('home')
